3d5e4ef15220a07ca68a6427e21f60996a663fcf
Naresh <naresh0689@gmail.com>
2017-07-05 17:41:12 +0530

    1

--- src/practice/arrays/RemoveDuplicates.java
+++ src/practice/arrays/RemoveDuplicates.java
@@ -1,9 +1,16 @@
 package practice.arrays;
 
 public class RemoveDuplicates {
+	
+	public static void removeDuplicates1(int[] in){
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int N = Integer.parseInt(args[0]);
+		int[] in = new int[N];
+		removeDuplicates1(in);
 
 	}
 
